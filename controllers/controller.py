ecd91371fa3ae335fc17fef38ce22ab04b6fdbce
briPod <33463220+br1Pod@users.noreply.github.com>
2021-08-05 14:00:07 +0100

    folder and main files setup

---
+++ controllers/controller.py
@@ -0,0 +1,4 @@
+from flask import render_template, request
+from app import app
+from models.event import *
+from models.event_list import events
---
+++ models/event.py
@@ -0,0 +1,10 @@
+class Event:
+
+    def __init__(self, date, title, capacity, location, description):
+        self.date = date
+        self.title = title
+        self.capacity = capacity 
+        self.location = location
+        self.description = description
+
+        
