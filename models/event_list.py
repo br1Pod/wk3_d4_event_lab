36d0695ee1959b4d29b4c550893b401d967acaf1
briPod <33463220+br1Pod@users.noreply.github.com>
2021-08-05 14:10:16 +0100

    events setup

--- models/event_list.py
+++ models/event_list.py
@@ -1,3 +1,7 @@
 from models.event import *
 
-event1 = Event()
+event1 = Event("10th Aug", "Ross Noble - Standup", 250, "The Stand", "Gags n stuff")
+event2 = Event("17th Aug", "Billy O'Shea - Ceilidh", 450, "Pleasance", "Dancin n stuff")
+event3 = Event("24th Aug", "Rapunzel - Panto", 1250, "The Lyceum", "Plays n stuff")
+
+events = [event1, event2, event3]
