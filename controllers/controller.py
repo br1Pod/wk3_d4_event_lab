3e63a57f7f5b7ab8e612746b8b9f9c6c96441023
briPod <33463220+br1Pod@users.noreply.github.com>
2021-08-05 14:26:51 +0100

    event list shows, css started

--- controllers/controller.py
+++ controllers/controller.py
@@ -2,3 +2,10 @@ from flask import render_template, request
 from app import app
 from models.event import *
 from models.event_list import events
+
+@app.route("/events")
+def index():
+    return render_template("index.html", title="home", events = events)
+
+
+    
